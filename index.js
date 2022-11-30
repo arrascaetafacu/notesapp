565781ef98c73b915794974c75cb75b8cb6a8485
Facu Arrascaeta <arrascaetafacu@outlook.com>
2022-11-30 19:45:06 -0300

    frontend build

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json()) // json-parser
 app.use(requestLogger)
